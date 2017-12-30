e7eeb6d7c786773e7d04be61a3830d81dab0b2cd
cksajil <sajilck@gmail.com>
2017-12-30 16:12:18 +0530

    Add pull examples

---
+++ Codes/Downloader.r
@@ -0,0 +1,9 @@
+# Download Data From URLs
+
+ad <- read.csv("http://www-bcf.usc.edu/~gareth/ISL/Advertising.csv")
+head(ad)
+
+
+mydat <- fread("http://www.stats.ox.ac.uk/pub/datasets/csb/ch11b.dat")
+head(mydat)
+
