133f1483d6363b7c7339aee509c3701a5e476684
SAJIL C K <sajilck@gmail.com>
2017-12-31 10:52:22 +0530

    Add Excel File

--- Codes/Downloader.r
+++ Codes/Downloader.r
@@ -7,3 +7,5 @@ head(ad)
 mydat <- fread("http://www.stats.ox.ac.uk/pub/datasets/csb/ch11b.dat")
 head(mydat)
 
+sample <- read.table(".\\DataSets\\SampleData.csv", sep =",", header= TRUE)
+head(sample)
--- Codes/Downloader2.r
+++ Codes/Downloader2.r
@@ -4,7 +4,15 @@ if(!file.exists(".\\DataSets\\SampleData.csv")){
   URL = "https://raw.githubusercontent.com/cksajil/DataWranglingWithR/master/DataSets/SampleData.csv"
   download.file(URL, destfile = ".\\Datasets\\SampleData.csv", method ="curl")
   dateofDownload<-date()
-  }
+}
+
+
+if(!file.exists(".\\DataSets\\BaltimoreCameraData.xlsx")){
+  URL = "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.tsv?accessType=DOWNLOAD"
+  download.file(URL, destfile = ".\\Datasets\\BaltimoreCameraData.xlsx", method ="curl")
+  dateofDownload<-date()
+}
+
 
 
 list.files(".\\Datasets")
